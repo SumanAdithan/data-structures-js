0414371ee1c098ebbea86b15b45f80bf3bfd652d
SumanAdithan <sumanadithang34@gmail.com>
2025-09-28 22:50:20 +0530

    feat: add array

---
+++ Build-in-ds/1.Array/index.js
@@ -0,0 +1,11 @@
+const arr = [1, 2, 3, 'Hello'];
+arr.push(4); // add 4 at end
+arr.unshift(0); // add 0 at beginning
+arr.pop(); // remove item from end
+arr.shift(); // remove item from beginning
+
+for (const item of arr) {
+    console.log(item);
+}
+
+// map, filter, reduce, concat, slice and splice
---
+++ Build-in-ds/1.Array/overview.js
@@ -0,0 +1,20 @@
+// Array
+// An array is a data structure that can hold a collection of values
+// Array can contain a mix of different data types. You can store strings, booleans, numbers or even objects all in the same array
+// Arrays are resizable. You don't have to declare the size of an array before creating it
+// JavaScript arrays are zero-indexed and the insertion order is maintained
+// Arrays are iterables. They can be used with a for of loop
+
+// Array - Big-O time complexity
+// Insert/remove from end - O(1) ==> the time complexity is constant
+
+// Insert/remove from beginning - O(n) ==> the time complexity is linear,
+// because the index has to be reset for every remaining element in the array
+
+// Access - O(1) ==> the time complexity is constant, because accessing position 1 and 1001 have no difference
+// Search - O(n) ==> the time complexity is linear, because searching the element can be the last one in the array
+
+// Push/pop - O(1) ==> the time complexity is constant
+// Shift/unshift/concat/slice/splice - O(n) ==> the time complexity is linear
+
+// forEach/map/filter/reduce - O(n) ==> the time complexity is linear
