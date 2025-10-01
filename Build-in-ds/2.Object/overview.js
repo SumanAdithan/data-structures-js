7b9700bb3f9d667f5222073e37c7b11cc1f48181
SumanAdithan <sumanadithang34@gmail.com>
2025-10-01 09:06:39 +0530

    feat: add object

---
+++ Build-in-ds/2.Object/overview.js
@@ -0,0 +1,15 @@
+// Object
+// An object is an unordered collection of key-value pairs. The key must either be a string or symbol data type where as the value can be any data type
+// To retrieve a value, you can use the corresponding key. This can be achieved using the dot notation or bracket notation
+// An object is not an iterable. You cannot use it with a for of loop
+
+// Object - Big-O time complexity
+// Insert - O(1) ==> the time complexity is constant
+// Remove - O(1) ==> the time complexity is constant
+
+// Access - O(1) ==> the time complexity is constant
+// Search - O(n) ==> the time complexity is linear
+
+// Object.keys() - O(n) ==> the time complexity is linear
+// Object.values() - O(n) ==> the time complexity is linear
+// Object.entries() - O(n) ==> the time complexity is linear
